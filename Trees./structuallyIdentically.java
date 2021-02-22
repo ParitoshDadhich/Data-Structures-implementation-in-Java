ce3140430885834d6726821a63346cc6e4079fb1
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2021-02-22 20:45:35 +0530

    structurally identical trees
    Sample Input 1 :
    10 3 20 30 40 2 40 50 0 0 0 0 
    10 3 20 30 40 2 40 50 0 0 0 0
    Sample Output 1 :
    true
    Sample Input 2 :
    10 3 20 30 40 2 40 50 0 0 0 0 
    10 3 2 30 40 2 40 50 0 0 0 0
    Sample Output 2:
    false

---
+++ Trees./structuallyIdentically.java
@@ -0,0 +1,140 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+ class QueueEmptyException extends Exception {
+}
+
+ class QueueUsingLL<T> {
+
+	class Node<T> {
+		T data;
+		Node<T> next;
+		Node(T data){
+			this.data = data;
+		}
+	}
+	
+	private Node<T> head;
+	private Node<T> tail;
+	private int size = 0;
+
+	public int size(){
+		return size;
+	}
+
+	public boolean isEmpty(){
+		if(size == 0){
+			return true;
+		}
+		return false;
+	}
+
+	public T front() throws QueueEmptyException{
+		if(size == 0){
+			QueueEmptyException e = new QueueEmptyException();
+			throw e;
+		}
+
+		return head.data;
+	}
+
+
+	public void enqueue(T element){
+		Node<T> newNode = new Node<T>(element);
+
+		if(head == null){
+			head = newNode;
+			tail = newNode;
+		}
+		else{
+			tail.next = newNode;
+			tail = newNode;
+		}
+
+		size++;
+	}
+
+	public T dequeue() throws QueueEmptyException{
+		if(head == null){
+			QueueEmptyException e = new QueueEmptyException();
+			throw e;
+		}
+		if(head == tail){
+			tail = null;
+		}
+		T temp = head.data;
+		head = head.next;
+		size--;
+		return temp;
+	}
+}
+
+   class TreeNode<T> {
+		T data;
+		ArrayList<TreeNode<T>> children;
+
+		TreeNode(T data){
+			this.data = data;
+			children = new ArrayList<TreeNode<T>>();
+		}
+	}
+ 
+public class Main {
+
+	
+
+	static Scanner s = new Scanner(System.in);
+
+	public static TreeNode<Integer> takeInputLevelWise(){
+		QueueUsingLL<TreeNode<Integer>> pendingNodes = new QueueUsingLL<TreeNode<Integer>>();  // Queue of node that are entered themselves but their children aren't added yet
+		int rootData = s.nextInt();
+		TreeNode<Integer> root = new TreeNode<Integer>(rootData);
+		pendingNodes.enqueue(root);
+		while(!pendingNodes.isEmpty()){
+			TreeNode<Integer> currentNode;
+			try {
+				currentNode = pendingNodes.dequeue();
+				int numChild = s.nextInt();
+				for(int i = 0 ; i < numChild; i++){
+					int currentChild = s.nextInt();
+					TreeNode<Integer> childNode = new TreeNode<Integer>(currentChild);
+					pendingNodes.enqueue(childNode);
+					currentNode.children.add(childNode);
+				}
+			} catch (QueueEmptyException e) {
+			}
+		}
+		return root;
+	}
+
+
+	public static void main(String[] args) {
+		TreeNode<Integer> root1 =  takeInputLevelWise();
+		TreeNode<Integer> root2 = takeInputLevelWise();
+		System.out.println(Solution.checkIdentical(root1, root2));
+	}
+
+}
+
+
+
+public class Solution {
+ 	
+	public static boolean checkIdentical(TreeNode<Integer> root1, TreeNode<Integer> root2){
+		if(root1 == null && root2 == null) return true;
+        
+        if(root1 == null || root2 == null || root1.data != root2.data)
+            return false;
+        
+        if(root1.children.size() != root2.children.size())
+            return false;
+        
+        for(int i=0; i<root1.children.size(); i++){
+            if(!checkIdentical(root1.children.get(i), root2.children.get(i)))
+                return false;
+        }
+        
+        return true;
+	}
+}
