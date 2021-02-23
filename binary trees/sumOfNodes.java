0c8357b12a6eb8ba536062f6e17e3f25fdd4ca76
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2021-02-24 01:42:45 +0530

    sum of nodes in a binary tree
    Constraints:
    1 <= N <= 10^6
    Where N is the total number of nodes in the binary tree.
    
    Time Limit: 1 sec
    Sample Input 1:
    2 3 4 6 -1 -1 -1 -1 -1
    Sample Output 1:
     15
    Sample Input 2:
    1 2 3 4 5 6 7 -1 -1 -1 -1 -1 -1 -1 -1
    Sample Output 2:
     28

---
+++ binary trees/sumOfNodes.java
@@ -0,0 +1,157 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+
+class QueueEmptyException extends Exception {
+
+}
+
+ class QueueUsingLL<T> {
+
+	class Node<T> {
+		T data;
+		Node<T> next;
+		Node(T data){
+			this.data = data;
+		}
+	}
+	
+	private Node<T> head;
+	private Node<T> tail;
+	private int size = 0;
+
+	public int size(){
+		return size;
+	}
+
+	public boolean isEmpty(){
+		if(size == 0){
+			return true;
+		}
+		return false;
+	}
+
+	public T front() throws QueueEmptyException{
+		if(size == 0){
+			QueueEmptyException e = new QueueEmptyException();
+			throw e;
+		}
+
+		return head.data;
+	}
+
+
+	public void enqueue(T element){
+		Node<T> newNode = new Node<T>(element);
+
+		if(head == null){
+			head = newNode;
+			tail = newNode;
+		}
+		else{
+			tail.next = newNode;
+			tail = newNode;
+		}
+
+		size++;
+	}
+
+	public T dequeue() throws QueueEmptyException{
+		if(head == null){
+			QueueEmptyException e = new QueueEmptyException();
+			throw e;
+		}
+		if(head == tail){
+			tail = null;
+		}
+		T temp = head.data;
+		head = head.next;
+		size--;
+		return temp;
+	}
+}
+
+
+
+
+class BinaryTreeNode<T> {
+	T data;
+	BinaryTreeNode<T> left;
+	BinaryTreeNode<T> right;
+
+	public BinaryTreeNode(T data) {
+		this.data = data;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+public class Runner {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	public static BinaryTreeNode<Integer> takeInput() throws NumberFormatException, IOException {
+		QueueUsingLL<BinaryTreeNode<Integer>>  pendingNodes = new QueueUsingLL<BinaryTreeNode<Integer>>(); 
+		int start = 0;
+
+		String[] nodeDatas = br.readLine().trim().split(" ");
+
+		int rootData = Integer.parseInt(nodeDatas[start]);
+		start += 1;
+
+		BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(rootData);
+		pendingNodes.enqueue(root);
+
+		while(!pendingNodes.isEmpty()){
+			BinaryTreeNode<Integer> currentNode;
+			try {
+				currentNode = pendingNodes.dequeue();
+			} catch (QueueEmptyException e) {
+				return null;
+			}
+
+			int leftChildData = Integer.parseInt(nodeDatas[start]);
+			start += 1;
+
+			if(leftChildData != -1){
+				BinaryTreeNode<Integer> leftChild = new BinaryTreeNode<Integer>(leftChildData);
+				currentNode.left = leftChild;
+				pendingNodes.enqueue(leftChild);
+			}
+
+			int rightChildData = Integer.parseInt(nodeDatas[start]);
+			start += 1;
+
+			if(rightChildData != -1){
+				BinaryTreeNode<Integer> rightChild = new BinaryTreeNode<Integer>(rightChildData);
+				currentNode.right = rightChild;
+				pendingNodes.enqueue(rightChild);
+			}
+		}
+
+		return root;
+	}
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BinaryTreeNode<Integer> root = takeInput();
+		System.out.println(Solution.getSum(root));
+	}
+}
+
+
+ public class Solution {
+
+	public static int getSum(BinaryTreeNode<Integer> root) {
+		if(root == null)
+            return 0;
+        
+        int ans = root.data;
+        ans += getSum(root.left);
+        ans += getSum(root.right);
+        
+        return ans;
+	}
+
+}
