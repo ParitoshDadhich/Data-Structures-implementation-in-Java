e11cfbf5f5be31818608a5522f77ef2da0f57af9
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2021-02-23 12:33:19 +0530

    node having sum of children and node is max #method2 (using pair class)
    Sample Input 1 :
    5 3 1 2 3 1 15 2 4 5 1 6 0 0 0 0
    Sample Output 1 :
    1

---
+++ Trees./M2nodeHavingSumOfChildrenAndMaxNode.java
@@ -0,0 +1,170 @@
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+ class QueueEmptyException extends Exception {
+}
+
+ class QueueUsingLL<T> {
+
+	class Node<T> {
+		T data;
+		Node<T> next;
+		Node(T data){
+			this.data = data;
+		}
+	}
+	
+	private Node<T> head;
+	private Node<T> tail;
+	private int size = 0;
+
+	public int size(){
+		return size;
+	}
+
+	public boolean isEmpty(){
+		if(size == 0){
+			return true;
+		}
+		return false;
+	}
+
+	public T front() throws QueueEmptyException{
+		if(size == 0){
+			QueueEmptyException e = new QueueEmptyException();
+			throw e;
+		}
+
+		return head.data;
+	}
+
+
+	public void enqueue(T element){
+		Node<T> newNode = new Node<T>(element);
+
+		if(head == null){
+			head = newNode;
+			tail = newNode;
+		}
+		else{
+			tail.next = newNode;
+			tail = newNode;
+		}
+
+		size++;
+	}
+
+	public T dequeue() throws QueueEmptyException{
+		if(head == null){
+			QueueEmptyException e = new QueueEmptyException();
+			throw e;
+		}
+		if(head == tail){
+			tail = null;
+		}
+		T temp = head.data;
+		head = head.next;
+		size--;
+		return temp;
+	}
+}
+
+   class TreeNode<T> {
+		T data;
+		ArrayList<TreeNode<T>> children;
+
+		TreeNode(T data){
+			this.data = data;
+			children = new ArrayList<TreeNode<T>>();
+		}
+	}
+ 
+public class Main {
+
+	
+
+	static Scanner s = new Scanner(System.in);
+
+		public static TreeNode<Integer> takeInputLevelWise(){
+		QueueUsingLL<TreeNode<Integer>> pendingNodes = new QueueUsingLL<TreeNode<Integer>>();  // Queue of node that are entered themselves but their children aren't added yet
+//		System.out.println("Enter root Data");
+		int rootData = s.nextInt();
+		TreeNode<Integer> root = new TreeNode<Integer>(rootData);
+		pendingNodes.enqueue(root);
+		while(!pendingNodes.isEmpty()){
+			TreeNode<Integer> currentNode;
+			try {
+				currentNode = pendingNodes.dequeue();
+//				System.out.println("Enter number of children of "+currentNode.data);
+				int numChild = s.nextInt();
+				for(int i = 0 ; i < numChild; i++){
+//					System.out.println("Enter "+ i + "th child of " + currentNode.data);
+					int currentChild = s.nextInt();
+					TreeNode<Integer> childNode = new TreeNode<Integer>(currentChild);
+					pendingNodes.enqueue(childNode);
+					currentNode.children.add(childNode);
+				}
+			} catch (QueueEmptyException e) {
+			}
+		}
+		return root;
+	}
+
+	public static void main(String[] args) {
+		TreeNode<Integer> root =  takeInputLevelWise();
+		TreeNode<Integer> ans = Solution.maxSumNode(root);
+		if(ans == null){
+			System.out.println(Integer.MIN_VALUE);
+		}else{
+			System.out.println(ans.data);
+		}
+		
+	}
+
+}
+
+
+public class Solution {
+	
+	static class Pair<T> {
+        int sum;
+        TreeNode<T> node;
+    }
+    
+	public static TreeNode<Integer> maxSumNode(TreeNode<Integer> root){
+        if(root == null) return null;
+        
+        return helper(root).node;
+    }
+    
+    private static Pair<Integer> helper(TreeNode<Integer> root){
+        
+        Pair<Integer> ans = new Pair<>();
+        ans.node = root;
+        ans.sum = sumOfNodes(root);
+        
+        for(TreeNode<Integer> child: root.children){
+            Pair<Integer> temp = helper(child);
+            
+            if(temp.sum > ans.sum){
+                ans.sum = temp.sum;
+                ans.node = temp.node;
+            }
+        }
+        
+        return ans;
+        
+    }
+    
+    
+    // calculates the sum of children;
+    private static int sumOfNodes(TreeNode<Integer> root){
+        int sum = root.data;
+        for(TreeNode<Integer> child: root.children)
+            sum += child.data;
+        
+        return sum;
+    }
+    
+}
