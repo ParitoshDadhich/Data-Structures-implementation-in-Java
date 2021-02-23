fb0b6f6553f535eee524f5b567ff7cb82d6e757c
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2021-02-23 18:19:11 +0530

    binary tree node class

---
+++ binary trees/binaryTreeNode.java
@@ -0,0 +1,11 @@
+package binaryTree;
+
+public class binaryTreeNode<T> {
+	T data;
+	public binaryTreeNode<T> left;
+	public binaryTreeNode<T> right;
+	
+	binaryTreeNode(T data){
+		this.data = data;
+	}
+}
