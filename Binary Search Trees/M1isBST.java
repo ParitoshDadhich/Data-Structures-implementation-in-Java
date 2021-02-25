3c298013fd0ceb94300e08f42164ccb0a6e6029e
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2021-02-26 05:13:20 +0530

    best case time o(nlogn) || worst case time o(n2)
    Constraints :
    Time Limit: 1 second
    Sample Input 1 :
    3 1 5 -1 2 -1 -1 -1 -1
    Sample Output 1 :
    true
    Sample Input 2 :
    5 2 10 0 1 -1 15 -1 -1 -1 -1 -1 -1
    Sample Output 2 :
    false

---
+++ Binary Search Trees/M1isBST.java
@@ -0,0 +1,197 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class QueueEmptyException extends Exception {
+
+}
+
+ class QueueUsingLL<T> {
+
+	class Node<T> {
+		T data;
+		Node<T> next;
+		Node(T data){
+			this.data = data;
+		}
+	}
+	
+	private Node<T> head;
+	private Node<T> tail;
+	private int size = 0;
+
+	public int size(){
+		return size;
+	}
+
+	public boolean isEmpty(){
+		if(size == 0){
+			return true;
+		}
+		return false;
+	}
+
+	public T front() throws QueueEmptyException{
+		if(size == 0){
+			QueueEmptyException e = new QueueEmptyException();
+			throw e;
+		}
+
+		return head.data;
+	}
+
+
+	public void enqueue(T element){
+		Node<T> newNode = new Node<T>(element);
+
+		if(head == null){
+			head = newNode;
+			tail = newNode;
+		}
+		else{
+			tail.next = newNode;
+			tail = newNode;
+		}
+
+		size++;
+	}
+
+	public T dequeue() throws QueueEmptyException{
+		if(head == null){
+			QueueEmptyException e = new QueueEmptyException();
+			throw e;
+		}
+		if(head == tail){
+			tail = null;
+		}
+		T temp = head.data;
+		head = head.next;
+		size--;
+		return temp;
+	}
+}
+
+
+class BinaryTreeNode<T> {
+	T data;
+	BinaryTreeNode<T> left;
+	BinaryTreeNode<T> right;
+
+	public BinaryTreeNode(T data) {
+		this.data = data;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+public class Runner {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	public static BinaryTreeNode<Integer> takeInput() throws NumberFormatException, IOException {
+		QueueUsingLL<BinaryTreeNode<Integer>>  pendingNodes = new QueueUsingLL<BinaryTreeNode<Integer>>(); 
+		int start = 0;
+
+		String[] nodeDatas = br.readLine().trim().split(" ");
+
+		if (nodeDatas.length == 1) {
+			return null;
+		}
+		
+		int rootData = Integer.parseInt(nodeDatas[start]);
+		start += 1;
+
+		BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(rootData);
+		pendingNodes.enqueue(root);
+
+		while(!pendingNodes.isEmpty()){
+			BinaryTreeNode<Integer> currentNode;
+			try {
+				currentNode = pendingNodes.dequeue();
+			} catch (QueueEmptyException e) {
+				return null;
+			}
+
+			int leftChildData = Integer.parseInt(nodeDatas[start]);
+			start += 1;
+
+			if(leftChildData != -1){
+				BinaryTreeNode<Integer> leftChild = new BinaryTreeNode<Integer>(leftChildData);
+				currentNode.left = leftChild;
+				pendingNodes.enqueue(leftChild);
+			}
+
+			int rightChildData = Integer.parseInt(nodeDatas[start]);
+			start += 1;
+
+			if(rightChildData != -1){
+				BinaryTreeNode<Integer> rightChild = new BinaryTreeNode<Integer>(rightChildData);
+				currentNode.right = rightChild;
+				pendingNodes.enqueue(rightChild);
+			}
+		}
+
+		return root;
+	}
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BinaryTreeNode<Integer> root = takeInput();
+
+        boolean ans = Solution.isBST(root);
+        
+        System.out.println(ans);
+
+	}
+}
+
+
+
+
+// NOTE
+/*
+	1. every element of BST must be less than root element;
+    2. every elemennt of BST must be greater than or equal to root element;
+*/
+
+// METHOD 1;
+
+// Time o(n*h)
+// in average case h = logn;
+// in worst case h = n;
+
+
+public class Solution {
+
+	public static boolean isBST(BinaryTreeNode<Integer> root) {
+		
+        if(root == null) return true;
+        
+        if(root.data < max(root.left))
+			return false;
+        
+        if(root.data >= min(root.right))
+		 	return false;
+        
+        boolean isLeftBST = isBST(root.left);
+        boolean isRightBST = isBST(root.right);
+        
+        return isLeftBST&&isRightBST;
+	}
+
+    private static int max(BinaryTreeNode<Integer> root){
+        
+        if(root == null) return Integer.MIN_VALUE;
+            
+        return Math.max(root.data, Math.max(max(root.left), max(root.right)));
+          
+    }
+    
+    private static int min(BinaryTreeNode<Integer> root){
+        if(root == null) 
+            return Integer.MAX_VALUE;
+        
+        return Math.min(root.data, Math.min(min(root.left), min(root.right)));
+    }
+    
+}
+
