e81f7588859f51463364250411b1867413b0e391
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2021-02-25 15:52:48 +0530

    search node in a BST
    Constraints:
    Time Limit: 1 second
    Sample Input 1 :
    8 5 10 2 6 -1 -1 -1 -1 -1 7 -1 -1
    2
    Sample Output 1 :
    true
    Sample Input 2 :
    8 5 10 2 6 -1 -1 -1 -1 -1 7 -1 -1
    12
    Sample Output 2 :
    false

---
+++ Binary Search Trees/searchNodeInBST.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+
+class BinaryTreeNode<T> {
+	T data;
+	BinaryTreeNode<T> left;
+	BinaryTreeNode<T> right;
+
+	public BinaryTreeNode(T data) {
+		this.data = data;
+	}
+}
+
+public class Main {
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));;
+	static StringTokenizer st;
+	
+	public static BinaryTreeNode<Integer> takeInput() throws IOException {
+		st = new StringTokenizer(br.readLine());
+		int rootData = Integer.parseInt(st.nextToken());
+		if (rootData == -1) {
+			return null;
+		}
+		Queue<BinaryTreeNode<Integer>> pendingNodes = new LinkedList<>();
+		BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(rootData);
+		pendingNodes.add(root);
+
+		while (!pendingNodes.isEmpty()) {
+			BinaryTreeNode<Integer> currentNode;
+			try {
+				currentNode = pendingNodes.remove();
+			} catch (Exception e) {
+				return null;
+			}
+			int leftChildData = Integer.parseInt(st.nextToken());
+			if (leftChildData != -1) {
+				BinaryTreeNode<Integer> leftChild = new BinaryTreeNode<Integer>(leftChildData);
+				currentNode.left = leftChild;
+				pendingNodes.add(leftChild);
+			}
+			int rightChildData = Integer.parseInt(st.nextToken());
+			if (rightChildData != -1) {
+				BinaryTreeNode<Integer> rightChild = new BinaryTreeNode<Integer>(rightChildData);
+				currentNode.right = rightChild;
+				pendingNodes.add(rightChild);
+			}
+		}
+		return root;
+	}
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BinaryTreeNode<Integer> root = takeInput();
+		int k = Integer.parseInt(br.readLine());
+		System.out.println(Solution.searchInBST(root,k));
+	}
+
+}
+
+
+
+public class Solution {
+	public static boolean searchInBST(BinaryTreeNode<Integer> root, int k) {
+		if(root == null) return false;
+        
+        if(root.data == k)
+            return true;
+        
+        if(root.data > k)
+            return searchInBST(root.left, k);
+        else
+            return searchInBST(root.right, k);
+	}
+}
