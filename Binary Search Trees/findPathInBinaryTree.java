ba6211ed60690d885f4d04ae2a6c9ef1a2471385
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2021-02-26 10:58:03 +0530

    find path in binary tree
    Constraints:
    Time Limit: 1 second   
    Sample Input 1:
    8 5 10 2 6 -1 -1 -1 -1 -1 7 -1 -1
    2
    Sample Output 1:
    2 5 8

---
+++ Binary Search Trees/findPathInBinaryTree.java
@@ -0,0 +1,174 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+class QueueEmptyException extends Exception {
+}
+
+class QueueUsingLL<T> {
+
+	class Node<T> {
+		T data;
+		Node<T> next;
+
+		Node(T data) {
+			this.data = data;
+		}
+	}
+
+	private Node<T> head;
+	private Node<T> tail;
+	private int size = 0;
+
+	public int size() {
+		return size;
+	}
+
+	public boolean isEmpty() {
+		if (size == 0) {
+			return true;
+		}
+		return false;
+	}
+
+	public T front() throws QueueEmptyException {
+		if (size == 0) {
+			QueueEmptyException e = new QueueEmptyException();
+			throw e;
+		}
+
+		return head.data;
+	}
+
+	public void enqueue(T element) {
+		Node<T> newNode = new Node<T>(element);
+
+		if (head == null) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			tail.next = newNode;
+			tail = newNode;
+		}
+
+		size++;
+	}
+
+	public T dequeue() throws QueueEmptyException {
+		if (head == null) {
+			QueueEmptyException e = new QueueEmptyException();
+			throw e;
+		}
+		if (head == tail) {
+			tail = null;
+		}
+		T temp = head.data;
+		head = head.next;
+		size--;
+		return temp;
+	}
+}
+
+class BinaryTreeNode<T> {
+	T data;
+	BinaryTreeNode<T> left;
+	BinaryTreeNode<T> right;
+
+	public BinaryTreeNode(T data) {
+		this.data = data;
+	}
+}
+
+public class Main {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+
+	public static BinaryTreeNode<Integer> takeInput() throws IOException {
+		st = new StringTokenizer(br.readLine());
+		int rootData = Integer.parseInt(st.nextToken());
+		if (rootData == -1) {
+			return null;
+		}
+		QueueUsingLL<BinaryTreeNode<Integer>> pendingNodes = new QueueUsingLL<BinaryTreeNode<Integer>>();
+		BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(rootData);
+		pendingNodes.enqueue(root);
+
+		while (!pendingNodes.isEmpty()) {
+			BinaryTreeNode<Integer> currentNode;
+			try {
+				currentNode = pendingNodes.dequeue();
+			} catch (QueueEmptyException e) {
+				return null;
+			}
+			int leftChildData = Integer.parseInt(st.nextToken());
+			if (leftChildData != -1) {
+				BinaryTreeNode<Integer> leftChild = new BinaryTreeNode<Integer>(leftChildData);
+				currentNode.left = leftChild;
+				pendingNodes.enqueue(leftChild);
+			}
+			int rightChildData = Integer.parseInt(st.nextToken());
+			if (rightChildData != -1) {
+				BinaryTreeNode<Integer> rightChild = new BinaryTreeNode<Integer>(rightChildData);
+				currentNode.right = rightChild;
+				pendingNodes.enqueue(rightChild);
+			}
+		}
+		return root;
+	}
+
+	public static void main(String[] args) throws IOException {
+		BinaryTreeNode<Integer> root = takeInput();
+		int k = Integer.parseInt(br.readLine());
+		ArrayList<Integer> output = Solution.getPath(root, k);
+		if(output != null) {
+			for(int i : output) {
+				System.out.print(i+" ");
+			}
+		}
+	}
+
+}
+
+
+
+import java.util.ArrayList;
+
+public class Solution {
+
+	public static ArrayList<Integer> getPath(BinaryTreeNode<Integer> root, int data){
+		if(root == null)
+        	return null;
+        
+        if(root.data == data){
+            ArrayList<Integer> output = new ArrayList<>();
+            output.add(root.data);
+            return output;
+        }
+        
+ 
+            ArrayList<Integer> leftOutput = getPath(root.left, data);
+            
+            if(leftOutput != null){
+            	leftOutput.add(root.data);
+            	return leftOutput;
+    		}
+ 
+        
+ 
+            ArrayList<Integer> rightOutput = getPath(root.right, data);
+            
+            if(rightOutput != null){
+            	rightOutput.add(root.data);
+            	return rightOutput;
+            }
+   
+        
+        else{
+            return null;
+        }
+	}
+}
+
