0a75bededc89da5a5e1886f7c8e02fde28015b82
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2021-02-27 01:18:53 +0530

    lca of BST || time o(logn) || method 2
    Constraints:
    Time Limit: 1 second
    Sample Input 1:
    8 5 10 2 6 -1 -1 -1 -1 -1 7 -1 -1
    2 10
    Sample Output 1:
    8
    Sample Input 2:
    8 5 10 2 6 -1 -1 -1 -1 -1 7 -1 -1
    2 6
    Sample Output 2:
    5
    Sample Input 3:
    8 5 10 2 6 -1 -1 -1 -1 -1 7 -1 -1
    12 78
    Sample Output 3:
    -1

---
+++ Binary Search Trees/M2_lcaOfBST.java
@@ -0,0 +1,146 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Scanner;
+import java.util.StringTokenizer;
+
+class QueueEmptyException extends Exception {
+}
+
+class QueueUsingLL<T> {
+
+	class Node<T> {
+		T data;
+		Node<T> next;
+
+		Node(T data) {
+			this.data = data;
+		}
+	}
+
+	private Node<T> head;
+	private Node<T> tail;
+	private int size = 0;
+
+	public int size() {
+		return size;
+	}
+
+	public boolean isEmpty() {
+		if (size == 0) {
+			return true;
+		}
+		return false;
+	}
+
+	public T front() throws QueueEmptyException {
+		if (size == 0) {
+			QueueEmptyException e = new QueueEmptyException();
+			throw e;
+		}
+
+		return head.data;
+	}
+
+	public void enqueue(T element) {
+		Node<T> newNode = new Node<T>(element);
+
+		if (head == null) {
+			head = newNode;
+			tail = newNode;
+		} else {
+			tail.next = newNode;
+			tail = newNode;
+		}
+
+		size++;
+	}
+
+	public T dequeue() throws QueueEmptyException {
+		if (head == null) {
+			QueueEmptyException e = new QueueEmptyException();
+			throw e;
+		}
+		if (head == tail) {
+			tail = null;
+		}
+		T temp = head.data;
+		head = head.next;
+		size--;
+		return temp;
+	}
+}
+
+class BinaryTreeNode<T> {
+	T data;
+	BinaryTreeNode<T> left;
+	BinaryTreeNode<T> right;
+
+	public BinaryTreeNode(T data) {
+		this.data = data;
+	}
+}
+
+public class Main {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	static StringTokenizer st;
+
+	public static BinaryTreeNode<Integer> takeInput() throws IOException {
+		st = new StringTokenizer(br.readLine());
+		int rootData = Integer.parseInt(st.nextToken());
+		if (rootData == -1) {
+			return null;
+		}
+		QueueUsingLL<BinaryTreeNode<Integer>> pendingNodes = new QueueUsingLL<BinaryTreeNode<Integer>>();
+		BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(rootData);
+		pendingNodes.enqueue(root);
+
+		while (!pendingNodes.isEmpty()) {
+			BinaryTreeNode<Integer> currentNode;
+			try {
+				currentNode = pendingNodes.dequeue();
+			} catch (QueueEmptyException e) {
+				return null;
+			}
+			int leftChildData = Integer.parseInt(st.nextToken());
+			if (leftChildData != -1) {
+				BinaryTreeNode<Integer> leftChild = new BinaryTreeNode<Integer>(leftChildData);
+				currentNode.left = leftChild;
+				pendingNodes.enqueue(leftChild);
+			}
+			int rightChildData = Integer.parseInt(st.nextToken());
+			if (rightChildData != -1) {
+				BinaryTreeNode<Integer> rightChild = new BinaryTreeNode<Integer>(rightChildData);
+				currentNode.right = rightChild;
+				pendingNodes.enqueue(rightChild);
+			}
+		}
+		return root;
+	}
+
+	public static void main(String[] args) throws IOException {
+		BinaryTreeNode<Integer> root = takeInput();
+		st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		System.out.println(Solution.getLCA(root, a, b));
+	}
+
+}
+
+
+public class Solution {
+ 
+	public static int lcaInBST(BinaryTreeNode<Integer> root , int a , int b){
+		if(root == null) return -1;
+        
+         
+        if(root.data > a && root.data > b)
+            return lcaInBST(root.left, a, b);
+        if(root.data < a && root.data < b)
+            return lcaInBST(root.right, a, b);
+
+        return root.data;
+	}
+}
