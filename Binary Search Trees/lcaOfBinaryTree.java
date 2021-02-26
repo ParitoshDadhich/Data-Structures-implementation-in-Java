847caa3ddd81ba200fe379b17ef8841df6a87e13
Paritosh Dadhich <69232753+ParitoshDadhich@users.noreply.github.com>
2021-02-27 00:57:15 +0530

    lca of binary tree || time o(n)
    Constraints:
    Time Limit: 1 second
    Sample Input 1:
    5 10 6 2 3 -1 -1 -1 -1 -1 9 -1 -1
    2 10
    Sample Output 1:
    10
    Sample Input 2:
    5 10 6 2 3 -1 -1 -1 -1 -1 9 -1 -1
    2 6
    Sample Output 2:
    5
    Sample Input 3:
    5 10 6 2 3 -1 -1 -1 -1 -1 9 -1 -1
    12 78
    Sample Output 3:
    -1

---
+++ Binary Search Trees/lcaOfBinaryTree.java
@@ -0,0 +1,177 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+class QueueEmptyException extends Exception {
+
+}
+
+ class QueueUsingLL<T> {
+
+	class Node<T> {
+		T data;
+		Node<T> next;
+		Node(T data){
+			this.data = data;
+		}
+	}
+	
+	private Node<T> head;
+	private Node<T> tail;
+	private int size = 0;
+
+	public int size(){
+		return size;
+	}
+
+	public boolean isEmpty(){
+		if(size == 0){
+			return true;
+		}
+		return false;
+	}
+
+	public T front() throws QueueEmptyException{
+		if(size == 0){
+			QueueEmptyException e = new QueueEmptyException();
+			throw e;
+		}
+
+		return head.data;
+	}
+
+
+	public void enqueue(T element){
+		Node<T> newNode = new Node<T>(element);
+
+		if(head == null){
+			head = newNode;
+			tail = newNode;
+		}
+		else{
+			tail.next = newNode;
+			tail = newNode;
+		}
+
+		size++;
+	}
+
+	public T dequeue() throws QueueEmptyException{
+		if(head == null){
+			QueueEmptyException e = new QueueEmptyException();
+			throw e;
+		}
+		if(head == tail){
+			tail = null;
+		}
+		T temp = head.data;
+		head = head.next;
+		size--;
+		return temp;
+	}
+}
+
+
+class BinaryTreeNode<T> {
+	T data;
+	BinaryTreeNode<T> left;
+	BinaryTreeNode<T> right;
+
+	public BinaryTreeNode(T data) {
+		this.data = data;
+		this.left = null;
+		this.right = null;
+	}
+}
+
+public class Runner {
+
+	static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+	public static BinaryTreeNode<Integer> takeInput() throws NumberFormatException, IOException {
+		QueueUsingLL<BinaryTreeNode<Integer>>  pendingNodes = new QueueUsingLL<BinaryTreeNode<Integer>>(); 
+		int start = 0;
+
+		String[] nodeDatas = br.readLine().trim().split(" ");
+
+		if (nodeDatas.length == 1) {
+			return null;
+		}
+		
+		int rootData = Integer.parseInt(nodeDatas[start]);
+		start += 1;
+
+		BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(rootData);
+		pendingNodes.enqueue(root);
+
+		while(!pendingNodes.isEmpty()){
+			BinaryTreeNode<Integer> currentNode;
+			try {
+				currentNode = pendingNodes.dequeue();
+			} catch (QueueEmptyException e) {
+				return null;
+			}
+
+			int leftChildData = Integer.parseInt(nodeDatas[start]);
+			start += 1;
+
+			if(leftChildData != -1){
+				BinaryTreeNode<Integer> leftChild = new BinaryTreeNode<Integer>(leftChildData);
+				currentNode.left = leftChild;
+				pendingNodes.enqueue(leftChild);
+			}
+
+			int rightChildData = Integer.parseInt(nodeDatas[start]);
+			start += 1;
+
+			if(rightChildData != -1){
+				BinaryTreeNode<Integer> rightChild = new BinaryTreeNode<Integer>(rightChildData);
+				currentNode.right = rightChild;
+				pendingNodes.enqueue(rightChild);
+			}
+		}
+
+		return root;
+	}
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		BinaryTreeNode<Integer> root = takeInput();
+        String[] strNums = br.readLine().trim().split(" ");
+        int a = Integer.parseInt(strNums[0]);
+        int b = Integer.parseInt(strNums[1]);
+
+		int h = Solution.getLCA(root, a, b);
+
+		System.out.println(h);
+	}
+}
+
+
+
+public class Solution {
+
+	static int getLCA(BinaryTreeNode<Integer> root, int a, int b) {
+ 		if(root == null) return -1;
+        
+        // when root data either equals to a or b;
+        if(root.data == a || root.data == b)
+            return root.data;
+        
+        int leftCall = getLCA(root.left, a, b);
+        int rightCall = getLCA(root.right, a, b);
+        
+        // when both left and right call are not equal to -1;
+        if(leftCall != -1 && rightCall != -1)
+            return root.data;
+        
+        // when right call is -1;
+        else if(leftCall != -1)
+            return leftCall;
+        
+        // when left call is -1;
+        else
+            return rightCall;
+        
+        
+	}
+}
